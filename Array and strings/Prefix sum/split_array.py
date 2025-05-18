1370e256864ffdead8ec056290f75ac85bd62361
Sathwik Gurugubelli <gurugubellisathwik@gmail.com>
2025-05-18 22:04:07 +0530

    Update split_array.py

--- Array and strings/Prefix sum/split_array.py
+++ Array and strings/Prefix sum/split_array.py
@@ -70,19 +70,3 @@ print(res)
 # We are still using the concept of a prefix sum as each value of leftSection represents the sum of a prefix.
 # We have simply replicated the functionality using an integer instead of an array.
 
-class Solutions:
-    def waysToSplitArrays(self, nums: List[int]) -> int:
-        ans = left_section = 0
-        total = sum(nums)
-
-        for i in range(len(nums) - 1):
-            left_section += nums[i]
-            right_section = total - left_section
-            if left_section >= right_section:
-                ans += 1
-
-        return ans
-    
-sol = Solutions()
-res=sol.waysToSplitArrays([10,4,-8,7])
-print(res)
