2c49c9dabf72ba68941fd9de2f1df3e5e1ebdd48
Sathwik Gurugubelli <gurugubellisathwik@gmail.com>
2025-05-20 18:11:27 +0530

    Update Assignment1.py

--- Array and strings/Prefix sum/Assignment1.py
+++ Array and strings/Prefix sum/Assignment1.py
@@ -39,13 +39,3 @@ s1=Solution()
 
 
 
-from typing import List
-class Solutions:
-    def runningSum(self, nums: List[int]) -> List[int]:
-        n=len(nums)
-        prefix=[nums[0]]
-        for i in range(1,n):
-            prefix.append(nums[i]+prefix[-1])
-        return prefix
-s2=Solutions()
-
